8dbcef85a74509b6937d463fa16e4d953e9ce44b
Nelson Venegas <nelson.venegasg@gmail.com>
2023-10-16 13:23:07 -0300

    test: testing features

--- server/router/app-routes.js
+++ server/router/app-routes.js
@@ -4,7 +4,9 @@ const router = Router()
 import { controller } from '../controllers/weather-controller.js'
 
 // app routes
-
+router.get('/', (req, res) => {
+  res.status(200).send('Ok')
+})
 router.get('/weather?', controller.getWeather)
 
 export default router
