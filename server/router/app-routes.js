3792a1af1874d3af9270ee4a993b0e50ef88c484
Nelson Venegas <nelson.venegasg@gmail.com>
2023-10-16 15:46:15 -0300

    fix: fixing var to g.deploy 1.4

--- server/router/app-routes.js
+++ server/router/app-routes.js
@@ -4,6 +4,9 @@ const router = Router()
 import { controller } from '../controllers/weather-controller.js'
 
 // app routes
+router.get('/', (req, res) => {
+  res.status(200).send('Backend funcionando')
+})
 router.get('/weather?', controller.getWeather)
 
 export default router
